ff69931f8efff7214dab848058f4e2d844edbdab
Aashna Sagar <112244344+aashna1504@users.noreply.github.com>
2026-06-02 11:48:12 +0530

    update base URL in axios configuration to use production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend base URL
+  baseURL: 'https://backend.gauyogkendr.com/api', // Backend base URL
 });
 
 // Request interceptor to append access token
